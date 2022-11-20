f806abfae4e2afd9b7c3b6457793bffecbb8d70a
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2022-11-20 14:45:56 +0000

    initial RyzenOS Bringup
    Credits:
    ArrowOS
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -94,7 +94,7 @@ public class NavigationBarInflaterView extends FrameLayout
     private static final String KEY_NAVIGATION_HINT =
             Settings.Secure.NAVIGATION_BAR_HINT;
     private static final String OVERLAY_NAVIGATION_HIDE_HINT =
-            "com.arrowos.overlay.customization.navbar.nohint";
+            "com.ryzenos.overlay.customization.navbar.nohint";
 
     protected LayoutInflater mLayoutInflater;
     protected LayoutInflater mLandscapeInflater;
